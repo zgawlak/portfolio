3682374c03f98cc39149c57653ef436a8b3ea162
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-31 14:11:07 -0400

    Create nav and mobile nav menu, add nav/menu icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
+import React, { useState } from 'react';
+import menu from './assets/icons/menu.svg';
+import close from './assets/icons/x.svg';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = (x) => {
+    setIsOpen(x);
+  }
+
   return (
     <div>
-      <header>
-        <h1>Header</h1>
-        <nav>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contacts">Contacts</a>
+      <header className='sticky flex justify-between px-8 py-6 border-b shadow-md shadow-purple-200'>
+        <h1 className='font-bold text-2xl'>zgawlak</h1>
+        <nav className='hidden md:flex gap-x-5 text-xl'>
+          <a href="#about" className='hover:text-purple-700'>About</a>
+          <a href="#projects" className='hover:text-purple-700'>Projects</a>
+          <a href="#contacts" className='hover:text-purple-700'>Contact</a>
         </nav>
+        <div className='md:hidden flex justify-center cursor-pointer'>
+          <img onClick={() => toggleMenu(true)} src={menu} alt="open menu" />
+        </div>
+        <div className={`${isOpen ? "right-0" : "-right-full"} w-full bg-white fixed top-0 h-full transition-all duration-300 z-20`}>
+          <img onClick={() => toggleMenu(false)} src={close} className='cursor-pointer absolute top-8 right-8 w-8' alt="close menu"/>
+          <div className='flex justify-center items-center p-5 h-full'>
+            <div className='flex flex-col gap-y-8 text-2xl text-center'>
+              <a href="#about" onClick={() => toggleMenu(false)} className='hover:text-purple-700'>About</a>
+              <a href="#projects" onClick={() => toggleMenu(false)} className='hover:text-purple-700'>Projects</a>
+              <a href="#contacts" onClick={() => toggleMenu(false)} className='hover:text-purple-700'>Contact</a>
+            </div>
+          </div>
+        </div>
       </header>
       <main>
         <div id="about">
